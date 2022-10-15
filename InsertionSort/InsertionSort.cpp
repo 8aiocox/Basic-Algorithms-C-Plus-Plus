82a3f48dfba65294d09e2ebf33690c009a2ecea8
unknown <paulbasse@outlook.com>
2022-10-15 01:51:51 +0100

    OOP InsertionSort

--- InsertionSort/InsertionSort.cpp
+++ InsertionSort/InsertionSort.cpp
@@ -1,30 +1,58 @@
 #include "InsertionSort.h"
+#include "../Utilities/Utilities.h"
 
-int main() {
-	unsigned int t = 0;
-	cout << "t is: " << --t << endl;
-	// Generate random numbers
-	vector<int> numbers = insertionsort::generate_random_numbers(10+rand()%20);
-	insertionsort::print_numbers(numbers);
-	vector<int> outcome;
-	bool end = false;
-	// Insert the first number anyway
-	outcome.push_back(*numbers.begin());
-	for (vector<int>::iterator h = numbers.begin()+1; h != numbers.end(); h++) {
-		vector<int>::iterator prev = outcome.end();
-		cout << " >>insert:" << *h << endl;
-		if (*(prev-1)>*h) {
-			while (true) {
-				if (prev == outcome.begin()) break;
-				if (*(prev - 1) < *h) {
-					break;
-				}
-				prev--;
-				cout << ">>prev:" << *prev << endl;
-			}
+class InsertionSort {
+public:
+	vector<int> numbers;
+
+
+	void print_numbers(std::vector<int> numbers) {
+		cout << "\nNumbers:";
+		for (vector<int>::iterator i = numbers.begin(); i != numbers.end(); i++) {
+			cout << " " << *i;
 		}
-		outcome.insert(prev, *h);
-		insertionsort::print_numbers(outcome);
+		cout << endl;
 	}
 
+	vector<int> generate_random_numbers(int range, bool unique = true) {
+		auto set = std::unordered_set<int>{};
+		while (set.size() < range) {
+			set.insert(5 + rand() % 101);
+		}
+		return { set.begin(), set.end() };
+	}
+
+	InsertionSort() {
+		// Generate random numbers
+		numbers = generate_random_numbers(10 + rand() % 20);
+		utilities::print_numbers(numbers);
+	}
+
+	void run() {
+		vector<int> outcome;
+		bool end = false;
+		// Insert the first number anyway
+		outcome.push_back(*numbers.begin());
+		for (vector<int>::iterator h = numbers.begin() + 1; h != numbers.end(); h++) {
+			vector<int>::iterator prev = outcome.end();
+			cout << " >>insert:" << *h << endl;
+			if (*(prev - 1) > *h) {
+				while (true) {
+					if (prev == outcome.begin()) break;
+					if (*(prev - 1) < *h) {
+						break;
+					}
+					prev--;
+					cout << ">>prev:" << *prev << endl;
+				}
+			}
+			outcome.insert(prev, *h);
+			utilities::print_numbers(outcome);
+		}
+	}
+};
+
+int main() {
+	InsertionSort* insertionSort = new InsertionSort();
+	insertionSort->run();
 }
--- InsertionSort/InsertionSort.h
+++ InsertionSort/InsertionSort.h
@@ -5,20 +5,5 @@ using namespace std;
 
 namespace insertionsort {
 
-	void print_numbers(std::vector<int> numbers) {
-		cout << "\nNumbers:";
-		for (vector<int>::iterator i = numbers.begin(); i != numbers.end(); i++) {
-			cout << " " << *i;
-		}
-		cout << endl;
-	}
-
-	vector<int> generate_random_numbers(int range, bool unique = true) {
-		auto set = std::unordered_set<int>{};
-		while (set.size() < range) {
-			set.insert(5 + rand() % 101);
-		}
-		return { set.begin(), set.end() };
-	}
 
 }
